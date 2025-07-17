daf7610b73d2f3d90778e83d5d4ad3761adb0788
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-07-17 11:06:03 -0300

    Adiciona comentários explicativos no index.js para melhorar a clareza do código

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'; // Importando React para poder usar JSX
+import ReactDOM from 'react-dom/client'; // Importando ReactDOM para renderizar o componente React no DOM
+import App from './App'; // Importando o componente App que contém a lógica do aplicativo
 
 // Importando a função App e redenderizando a mesmo no elemento html com id=root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App />); // Renderizando o componente App dentro do elemento com id 'root'
